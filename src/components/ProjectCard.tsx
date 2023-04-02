e093dfde7522236caa5ece261f027d92d01765f3
rajput-hemant <rajput.hemant2001@gmail.com>
2023-04-02 11:51:49 +0530

    added work section
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,71 @@
+import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+
+import { github } from "../assets";
+import { Project } from "../constants";
+import { fadeIn } from "../utils/motion";
+
+interface ProjectCardProps extends Project {
+  index: number;
+}
+
+const ProjectCard = ({
+  name,
+  image,
+  description,
+  index,
+  tags,
+  source_code_link,
+}: ProjectCardProps) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+export default ProjectCard;
